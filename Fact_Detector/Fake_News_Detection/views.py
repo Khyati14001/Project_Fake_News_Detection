8215f55006adffe91d6b42788b722bd54a69c5a6
Khyati <mistrykhyati14@gmail.com>
2021-04-14 12:35:31 +0530

    Registration db

--- Fact_Detector/Fake_News_Detection/views.py
+++ Fact_Detector/Fake_News_Detection/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+        return render(request,'home.html')
 
 def about(request):
     return render(request,'about.html')
 
 def Registration(request):
-    return render(request,'Registration.html')
+    if request.method == 'POST':
+        Userid = request.POST['User id']
+        Email = request.POST['Email']
+        Password = request.POST['Password']
+
+        user = User.objects.create_user(username=Userid,email=Email,password=Password)
+        user.save();
+        print('user created')
+        return redirect('/')
+    else:
+        return render(request,'Registration.html')
 
 def contact(request):
     return render(request,'contact.html')
